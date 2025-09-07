9b787f38616fe441f7692358cf55a6e86121599b
derekdeek <dereklian0789@gmail.com>
2025-09-06 18:15:22 -0700

    home and sign up

--- garden_planner/lib/main.dart
+++ garden_planner/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+const Color primaryColor = Color.fromARGB(255, 37, 49, 140);
+const Color secondaryColor = Color.fromARGB(240, 120, 143, 205);
+const Color accentColor = Color(0xFF43A047);
+const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
 void main() {
   runApp(const MyApp());
 }
@@ -14,14 +17,15 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-// --- This is the First Screen (Home Page) ---
+// --- This is the First Screen (Onboarding) ---
 class OnboardPage extends StatelessWidget {
+  
   const OnboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: backgroundColor,
       body: Stack(
         children: [
           // Background image
@@ -41,7 +45,7 @@ class OnboardPage extends StatelessWidget {
                 end: Alignment.bottomCenter,
                 colors: [
                   Colors.transparent,
-                  Colors.blue, // Change to your desired fade color
+                  primaryColor, 
                 ],
                 stops: [0, 1], // Adjust fade position
               ),
@@ -58,18 +62,25 @@ class OnboardPage extends StatelessWidget {
                 ),
                 SizedBox(height:40),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    
+                  },
                   style: ElevatedButton.styleFrom(
                     minimumSize: Size(200, 60), // width, height
-                    textStyle: TextStyle(fontSize: 24), // bigger text
+                    textStyle: TextStyle(fontSize: 24), 
                   ),
                   child: const Text('Sign in')),
                 SizedBox(height:30),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SignUpPage()),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                     minimumSize: Size(200, 60), // width, height
-                    textStyle: TextStyle(fontSize: 24), // bigger text
+                    textStyle: TextStyle(fontSize: 24), 
                   ),
                   child: const Text('Sign up')),
               ],
@@ -78,6 +89,113 @@ class OnboardPage extends StatelessWidget {
 
           ),
         ],
+      ),
+    );
+  }
+}
+class SignUpPage extends StatefulWidget{
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+class _SignUpPageState extends State<SignUpPage>{
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context){
+    
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height:100),
+            SizedBox(
+              width: 200, // desired width
+              height:200, // desired height
+              child: Image.asset(
+                'assets/icons/icon-blueberry.png',
+                fit: BoxFit.contain, // or BoxFit.cover, etc.
+              ),
+            ),
+            
+            SizedBox(height: 50),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                
+                labelText: 'Enter your email',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                
+                labelText: 'Enter your password',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                minimumSize: Size(200, 60), 
+                textStyle: TextStyle(fontSize: 24), 
+                
+              ),
+              child: Text('Create Account'),
+              
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+// -- home page
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: const Text('Welcome Back!', style: TextStyle(color: Colors.white)),
+        automaticallyImplyLeading: false,
+        toolbarHeight: 100,
+        shape: const RoundedRectangleBorder(
+          
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle, color: Colors.white),
+            onPressed: () {
+              // Handle settings button press
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Column(
+          
+          children: [
+            
+            
+          ],
+        ),
       ),
     );
   }
